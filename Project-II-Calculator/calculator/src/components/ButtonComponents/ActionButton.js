3cb68fd0473b46c245f51d8add7a6ddbbb754b08
jsmoot93 <47538435+jsmoot93@users.noreply.github.com>
2019-03-19 18:14:41 -0400

    mvp

--- Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Button.css';
+
+const ActionButton = props => {
+    return (
+        <button className={`bigger-button`}>
+            {props.text}
+        </button>
+    );
+};
+
+export default ActionButton;
